5c4d99724f8fdf9380644a8199bb46cd8f92ae04
AkashdeepSIN <azzy.virk.13@gmail.com>
2016-02-13 23:22:36 -0500

    first commint

---
+++ Script/app1.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// setup your IIFE(Immediately Invoked Function Ewperession) hint
+(function(){
+	
+	
+		var project;
+
+		project = document.getElementById("project");
+
+		project.innerHTML = "I made this website in December. It was about the car and the changes in technology. I made the home page, the page about different models of cars and I made the contact form to buy a car. Also, I made a page about the full information of website";
+	
+	
+})();
+
+(function(){
+	
+	
+		var thirdproject;
+
+		thirdproject = document.getElementById("thirdproject");
+
+		thirdproject.innerHTML = "I made this website in January. It was about the font 'Times New Roman'. I made a page about its history. A page for its users and the different sizes which are mostly using. And, I also made a page of references.";
+	
+	
+})();
+
+(function(){
+	
+	
+		var secondproject;
+
+		secondproject = document.getElementById("secondproject");
+
+		secondproject.innerHTML = "I made this website's page in December. It was about my movie's posture. I gave very peaceful background and concentrated on 3 words that are struggle, sacrifice and success. I gave the title according to the ambience and I used different fonts";
+	
+	
+})();
